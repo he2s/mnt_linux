644b8a759fb304758f1e71ab6ac3323b7584f9d5
HSC <henning.schmick@qosit.com>
2017-11-22 18:56:36 +0100

    Added stuff.

--- sh.sh
+++ sh.sh
@@ -1,1 +1,2 @@
 sudo docker run -ti --privileged --net=host --pid=host --cap-add sys_admin --cap-add sys_ptrace -e sysimage=/host -v /:/host -v /dev:/dev -v /run:/run mnt_linux /bin/bash
+# --pid=cotainer:12sh5
