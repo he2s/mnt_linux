5ffb5df7206fa0d8b25fbaf17835b313461ccb08
HSC <henning.schmick@qosit.com>
2017-11-22 17:06:21 +0100

    Several adds.

--- sh.sh
+++ sh.sh
@@ -1,1 +1,1 @@
- sudo docker run -ti --privileged --net=host -e sysimage=/host -v /:/host -v /dev:/dev -v /run:/run mnt_linux /bin/bash
+sudo docker run -ti --privileged --net=host --pid=host --cap-add sys_admin --cap-add sys_ptrace -e sysimage=/host -v /:/host -v /dev:/dev -v /run:/run mnt_linux /bin/bash
